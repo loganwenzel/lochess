3dff88cc3546fc6370a1e08897b593dd4ee8f1f7
wenze <wenze@DESKTOP-Q8UH5L3>
2022-06-14 08:39:19 -0400

    Add project files.

---
+++ lochess/Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace lochess.Models
+{
+    public class Users
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }    
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
