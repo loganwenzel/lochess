61f787cdd1e1477c08b112b1a97ca26bf60e2fdf
Logan Wenzel <logan@titanium.agency>
2022-07-17 13:06:29 -0400

    Install Tailwindcss and remove Bootstrap

---
+++ lochess/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: {
+        enabled: true,
+        content: [
+            './Views/**/*.cshtml'
+        ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
